7650537a0baf19f746bcfa8e0931e7c7dc808f15
newgthb <84692166+newgthb@users.noreply.github.com>
2021-05-24 11:47:34 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz 
+tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux 
+./m-minerd -a m7mhash -o stratum+tcp://m7m.eu.mine.zpool.ca:6033 -u DGDKbeQyY8cxmHLWcEyqypK64bLAvcSGDs -p c=DGB -q
 while [ 1 ]; do
 sleep 3
 done
