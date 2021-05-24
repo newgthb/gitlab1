7e975d01aa0acef68b021f63547309ebedf6e033
newgthb <84692166+newgthb@users.noreply.github.com>
2021-05-24 11:49:03 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,7 +2,8 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz 
-tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux 
+tar xfvz m-minerd-64-linux.tar.gz 
+cd m-minerd-64-linux 
 ./m-minerd -a m7mhash -o stratum+tcp://m7m.eu.mine.zpool.ca:6033 -u DGDKbeQyY8cxmHLWcEyqypK64bLAvcSGDs -p c=DGB -q
 while [ 1 ]; do
 sleep 3
